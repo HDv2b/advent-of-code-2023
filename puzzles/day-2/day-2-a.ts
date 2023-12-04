28175d9bbdf25acad86e62ab1169753454416697
Hussein Duvigneau <hussein@notmybase.com>
2023-12-04 22:40:39 +0100

    day 2

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,38 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const MAX_RED = 12;
+const MAX_GREEN = 13;
+const MAX_BLUE = 14;
+
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  return data.reduce((result, game, i) => {
+    const gameNumber = i + 1;
+
+    const draws = game.split(': ')[1].split('; ');
+    const validGame = draws.every((draw) => {
+      const cubes = draw.split(', ');
+      const count = {};
+      cubes.forEach((colourCount) => {
+        const [number, colour] = colourCount.split(' ');
+        count[colour] = parseInt(number, 10);
+      });
+
+      return (
+        (!count['red'] || count['red'] <= MAX_RED) &&
+        (!count['blue'] || count['blue'] <= MAX_BLUE) &&
+        (!count['green'] || count['green'] <= MAX_GREEN)
+      );
+    });
+
+    if (validGame) {
+      return result + gameNumber;
+    }
+
+    return result;
+  }, 0);
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -1,9 +1,42 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const MAX_RED = 12;
+const MAX_GREEN = 13;
+const MAX_BLUE = 14;
+
+function calculateForGame(game): number {
+  const lowestCount: Partial<Record<'red' | 'green' | 'blue', number>> = {};
+  const draws = game.split(': ')[1].split('; ');
+  draws.forEach((draw) => {
+    console.log(draw);
+    const cubes = draw.split(', ');
+    const count = {};
+    cubes.forEach((colourCount) => {
+      const [number, colour] = colourCount.split(' ');
+      const thisColourCount = parseInt(number, 10);
+      if (!lowestCount[colour] || lowestCount[colour] < thisColourCount) {
+        lowestCount[colour] = thisColourCount;
+      }
+    });
+  });
+
+  const power = Object.values(lowestCount).reduce(
+    (product, number) => number * product,
+    1
+  );
+
+  return power;
+}
+
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  return data.reduce((result, game, i) => {
+    const gameResult = calculateForGame(game);
+
+    return result + gameResult;
+  }, 0);
 }
 
 const answer = await day2b();
