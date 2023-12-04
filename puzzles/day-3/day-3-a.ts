9e8e227365a40a8e7bbdd60263c26f5202139ac1
Hussein Duvigneau <hussein@notmybase.com>
2023-12-04 23:40:18 +0100

    day 3a

--- puzzles/day-3/day-3-a.ts
+++ puzzles/day-3/day-3-a.ts
@@ -3,7 +3,48 @@ import chalk from 'chalk';
 
 export async function day3a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const connectedNodes: Boolean[][] = data.reduce(
+    (coords, row, rowI) => {
+      const newCoords = coords;
+      const symbolsInRow = row.matchAll(/([^\.\d])/g);
+
+      for (const symbol of symbolsInRow) {
+        const colI = symbol.index;
+        for (let r = rowI - 1; r <= rowI + 1; r++) {
+          for (let c = colI - 1; c <= colI + 1; c++) {
+            if (r > -1 && r < data.length && c > -1 && c < row.length) {
+              newCoords[r][c] = true;
+            }
+          }
+        }
+      }
+
+      return newCoords;
+    },
+    data.map((row) => row.split('').map(() => false))
+  );
+
+  const connectedNumbers = [];
+
+  data.forEach((row, rowI) => {
+    const numbersInRow = row.matchAll(/(\d+)/g);
+    for (const number of numbersInRow) {
+      let skip = false;
+      const c1 = number.index;
+      const c2 = c1 + number[0].length - 1;
+      for (let c = c1; c <= c2; c++) {
+        if (!skip) {
+          if (connectedNodes[rowI][c]) {
+            skip = true;
+            connectedNumbers.push(parseInt(number[0], 10));
+          }
+        }
+      }
+    }
+  });
+
+  return connectedNumbers.reduce((sum, n) => n + sum, 0);
 }
 
 const answer = await day3a();
