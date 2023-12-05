1ec6c840314c944cef99cce49d3f9189329516ff
Hussein Duvigneau <hussein@notmybase.com>
2023-12-05 23:24:27 +0100

    day 3b

--- puzzles/day-3/day-3-b.ts
+++ puzzles/day-3/day-3-b.ts
@@ -1,9 +1,61 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+function generateNumbersIndex(data: string[]): (false | number)[][] {
+  const falseGrid: (false | number)[][] = data.map((row) =>
+    row.split('').map(() => false)
+  );
+
+  return data.reduce((output, row, rowI) => {
+    const newGrid = output;
+
+    const numbersInRow = row.matchAll(/(\d+)/g);
+
+    for (const number of numbersInRow) {
+      for (let x = number.index; x < number.index + number[0].length; x++) {
+        newGrid[rowI][x] = parseInt(number[0], 10);
+      }
+    }
+
+    return newGrid;
+  }, falseGrid);
+}
+
+// this wouldn't work if any of the gears had two identical numbers!
+function getNumbersForGear(
+  data: (false | number)[][],
+  row: number,
+  col: number
+): number[] {
+  let foundNumbers = [];
+  for (let r = row - 1; r <= row + 1; r++) {
+    for (let c = col - 1; c <= col + 1; c++) {
+      if (r >= 0 && r < data.length && c >= 0 && c < data[r].length) {
+        if (data[r][c] && !foundNumbers.includes(data[r][c])) {
+          foundNumbers.push(data[r][c]);
+        }
+      }
+    }
+  }
+  return foundNumbers;
+}
+
 export async function day3b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data: string[] = await readData(dataPath);
+  const numbersIndex = generateNumbersIndex(data);
+  let total: number = 0;
+
+  data.forEach((row, rowI) => {
+    const gears = row.matchAll(/(\*)/g);
+    for (const gear of gears) {
+      const numbers = getNumbersForGear(numbersIndex, rowI, gear.index);
+      if (numbers.length === 2) {
+        total += numbers[0] * numbers[1];
+      }
+    }
+  });
+
+  return total;
 }
 
 const answer = await day3b();
--- shared.ts
+++ shared.ts
@@ -1,6 +1,6 @@
 import { readFile } from 'fs/promises';
 
-export async function readData(path?: string) {
+export async function readData(path?: string): Promise<string[]> {
   const fileName = path || process.argv[2];
   const data = (await readFile(fileName)).toString().split('\n').map(x => x.replace('\r', ''));
   return data;
