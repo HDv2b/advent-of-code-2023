394b473a4e43aee5472a2d7e19004571b26f2c2a
Hussein Duvigneau <hussein@notmybase.com>
2023-12-06 00:15:20 +0100

    day 4

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -3,7 +3,34 @@ import chalk from 'chalk';
 
 export async function day4a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const gamesScore = data
+    .map((row) => row.split(': ')[1].split('|'))
+    .map(([winningNumbers, givenNumbers]) => [
+      winningNumbers
+        .split(' ')
+        .filter(Boolean)
+        .map((n) => parseInt(n, 10)),
+      givenNumbers
+        .split(' ')
+        .filter(Boolean)
+        .map((n) => parseInt(n, 10)),
+    ])
+    .map(([winningNumbers, givenNumbers]) => {
+      let score = 0;
+      givenNumbers.forEach((g) => {
+        if (winningNumbers.includes(g)) {
+          score++;
+        }
+      });
+      if (score > 0) {
+        return Math.pow(2, score - 1);
+      }
+      return 0;
+    })
+    .reduce((total, score) => total + score, 0);
+
+  return gamesScore;
 }
 
 const answer = await day4a();
--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -3,7 +3,37 @@ import chalk from 'chalk';
 
 export async function day4b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const gamesScore = data
+    .map((row) => row.split(': ')[1].split('|'))
+    .map(([winningNumbers, givenNumbers]) => [
+      winningNumbers
+        .split(' ')
+        .filter(Boolean)
+        .map((n) => parseInt(n, 10)),
+      givenNumbers
+        .split(' ')
+        .filter(Boolean)
+        .map((n) => parseInt(n, 10)),
+    ])
+    .map(([winningNumbers, givenNumbers]) => {
+      let score = 0;
+      givenNumbers.forEach((g) => {
+        if (winningNumbers.includes(g)) {
+          score++;
+        }
+      });
+      return score;
+    });
+
+  const instances = gamesScore.map(() => 1);
+  for (let i = 0; i < instances.length; i++) {
+    const score = gamesScore[i];
+    for (let j = i + 1; j <= i + score; j++) {
+      instances[j] += instances[i];
+    }
+  }
+
+  return instances.reduce((total, n) => total+n, 0);
 }
 
 const answer = await day4b();
