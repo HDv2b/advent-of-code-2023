83e0c43e8551d8ff22f3baf5c7b4a980f4030858
Hussein Duvigneau <hussein@notmybase.com>
2023-12-08 09:35:00 +0100

    day 8  a

--- puzzles/day-8/day-8-a.ts
+++ puzzles/day-8/day-8-a.ts
@@ -3,7 +3,40 @@ import chalk from 'chalk';
 
 export async function day8a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let instructions = [];
+  let map: Record<string, string[]> = {};
+
+  for (let l = 0; l < data.length; l++) {
+    const line = data[l];
+
+    if (l === 0) {
+      instructions = line.split('');
+    }
+
+    if (l > 1) {
+      const wp = line.split(' = ');
+      map[wp[0]] = wp[1]
+        .split(', ')
+        .map((s) => s.replace('(', '').replace(')', ''));
+    }
+  }
+
+  let steps = 0;
+  let s = 0;
+  let currentStep = 'AAA';
+
+  while (currentStep !== 'ZZZ') {
+    steps += 1;
+    currentStep =
+      instructions[s] === 'L' ? map[currentStep][0] : map[currentStep][1];
+    s++;
+    if (s === instructions.length) {
+      s = 0
+    }
+  }
+
+  return steps;
 }
 
 const answer = await day8a();
