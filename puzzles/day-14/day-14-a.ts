c3e8bafcf42ab3ccdab6a35516c0229f356f783f
Hussein Duvigneau <hussein@notmybase.com>
2023-12-27 21:26:42 +0100

    day 14 a

--- puzzles/day-14/day-14-a.ts
+++ puzzles/day-14/day-14-a.ts
@@ -3,7 +3,29 @@ import chalk from 'chalk';
 
 export async function day14a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const tally = data[0].split('').map(() => 0);
+
+  const highestScore = data.length;
+  const nextScore = data[0].split('').map(() => highestScore);
+
+  // console.log(tally);
+  // console.log(nextScore);
+
+  data.forEach((row, rowI) => {
+    row.split('').forEach((col, i) => {
+      if (col === 'O') {
+        tally[i] += nextScore[i];
+        nextScore[i]--;
+      } else if (col === '#') {
+        nextScore[i] = highestScore - rowI - 1;
+      }
+    });
+    // console.log({ tally });
+    // console.log({ nextScore });
+  });
+
+  return tally.reduce((sum, n) => n + sum, 0);
 }
 
 const answer = await day14a();
