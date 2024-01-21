faa6a9dba7ba75cc0d3dfd69d82f1042e6ff6fd7
Hussein Duvigneau <hussein@notmybase.com>
2024-01-21 23:03:36 +0100

    day 15 pt 1

--- puzzles/day-15/day-15-a.ts
+++ puzzles/day-15/day-15-a.ts
@@ -1,9 +1,26 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+function getScore(chars: string): number {
+  let out = 0;
+
+  chars.split('').forEach((c) => {
+    const ascii = c.charCodeAt(0);
+    out += ascii;
+    out *= 17;
+    out %= 256;
+  });
+
+  return out;
+}
+
 export async function day15a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  return data[0]
+    .split(',')
+    .map(getScore)
+    .reduce((total: number, value: number) => value + total, 0);
 }
 
 const answer = await day15a();
